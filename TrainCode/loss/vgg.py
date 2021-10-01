840b4fcf77ae2ffa5d5affeaaf41cfff3b82c2a1
Naman Tuli <63744860+namantuli18@users.noreply.github.com>
2021-10-01 21:51:29 +0530

    Update vgg.py

--- TrainCode/loss/vgg.py
+++ TrainCode/loss/vgg.py
@@ -9,7 +9,7 @@ from torch.autograd import Variable
 class VGG(nn.Module):
     def __init__(self, conv_index, rgb_range=1):
         super(VGG, self).__init__()
-        vgg_features = models.vgg19(pretrained=True).features
+        vgg = models.vgg19(pretrained=True).features
         modules = [m for m in vgg_features]
         if conv_index == '22':
             self.vgg = nn.Sequential(*modules[:8])
