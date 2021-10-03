99d494b78584aaf1a3234bde8995c2f0a7007410
Naman Tuli <63744860+namantuli18@users.noreply.github.com>
2021-10-03 17:14:18 +0530

    Update custom_loss.py

--- TrainCode/loss/custom_loss.py
+++ TrainCode/loss/custom_loss.py
@@ -12,6 +12,8 @@ class custom_loss(nn.Module):
         self.C2 = (0.03 * 255)**2
 
     def forward(self, X, Y):
+        X=X.cpu()
+        Y=Y.cpu()
         X=X.detach().numpy()
         Y=Y.detach().numpy()
         kernel = cv2.getGaussianKernel(3, 1.5)
