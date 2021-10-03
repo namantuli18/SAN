c1486ee43066d04c07fadb6ff274b0978e33c06b
Naman Tuli <63744860+namantuli18@users.noreply.github.com>
2021-10-04 01:39:19 +0530

    Update custom_loss.py

--- TrainCode/loss/custom_loss.py
+++ TrainCode/loss/custom_loss.py
@@ -19,36 +19,36 @@ class custom_loss(nn.Module):
         Y=Y.detach().numpy()
         X=np.float64(X)
         Y=np.float64(Y)
-        img1=img1.cpu()
-        img2=img2.cpu()
-        img1=img1.detach().numpy()
-        img2=img2.detach().numpy()
-        img1=np.float64(img1)
-        img2=np.float64(img2)
+        X=X.cpu()
+        Y=Y.cpu()
+        X=X.detach().numpy()
+        Y=Y.detach().numpy()
+        X=np.float64(X)
+        Y=np.float64(Y)
 
         batch_loss=[]
-        for cnt,i in enumerate(img1):
+        for cnt,i in enumerate(X):
             # print(img1[cnt].shape,img2[cnt].shape)
-            h=img1[cnt].shape[2]
-            w=img1[cnt].shape[1]
-            c=img1[cnt].shape[0]
+            h=X[cnt].shape[2]
+            w=X[cnt].shape[1]
+            c=X[cnt].shape[0]
 
 
-            X=img1[cnt].reshape((w,h,c))
-            Y=img2[cnt].reshape((w,h,c))
+            x=X[cnt].reshape((w,h,c))
+            y=Y[cnt].reshape((w,h,c))
 
 
             kernel = cv2.getGaussianKernel(3, 1.5)
             window = np.outer(kernel, kernel.transpose())
 
-            mu1 = cv2.filter2D(X, -1, window)[5:-5, 5:-5]  # valid
-            mu2 = cv2.filter2D(Y, -1, window)[5:-5, 5:-5]
+            mu1 = cv2.filter2D(x, -1, window)[5:-5, 5:-5]  # valid
+            mu2 = cv2.filter2D(y, -1, window)[5:-5, 5:-5]
             mu1_sq = mu1**2
             mu2_sq = mu2**2
             mu1_mu2 = mu1 * mu2
-            sigma1_sq = cv2.filter2D(X**2, -1, window)[5:-5, 5:-5] - mu1_sq
-            sigma2_sq = cv2.filter2D(Y**2, -1, window)[5:-5, 5:-5] - mu2_sq
-            sigma12 = cv2.filter2D(X * Y, -1, window)[5:-5, 5:-5] - mu1_mu2
+            sigma1_sq = cv2.filter2D(x**2, -1, window)[5:-5, 5:-5] - mu1_sq
+            sigma2_sq = cv2.filter2D(y**2, -1, window)[5:-5, 5:-5] - mu2_sq
+            sigma12 = cv2.filter2D(x * y, -1, window)[5:-5, 5:-5] - mu1_mu2
 
 
 
