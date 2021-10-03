3e7e947cc096f8aee34e1c22109af7670b4cbbd3
Naman Tuli <63744860+namantuli18@users.noreply.github.com>
2021-10-04 00:17:50 +0530

    Update custom_loss.py

--- TrainCode/loss/custom_loss.py
+++ TrainCode/loss/custom_loss.py
@@ -21,8 +21,8 @@ class custom_loss(nn.Module):
         kernel = cv2.getGaussianKernel(3, 1.5)
         window = np.outer(kernel, kernel.transpose())
 
-        mu1 = cv2.filter2D(img1, -1, window)[5:-5, 5:-5]  # valid
-        mu2 = cv2.filter2D(img2, -1, window)[5:-5, 5:-5]
+        mu1 = cv2.filter2D(X, -1, window)[5:-5, 5:-5]  # valid
+        mu2 = cv2.filter2D(Y, -1, window)[5:-5, 5:-5]
         mu1_sq = mu1**2
         mu2_sq = mu2**2
         mu1_mu2 = mu1 * mu2
