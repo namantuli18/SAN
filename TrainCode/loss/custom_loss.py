3692f675674ef6c00e7d7921ce618e507048382e
Naman Tuli <63744860+namantuli18@users.noreply.github.com>
2021-11-13 01:50:01 +0530

    Update custom_loss.py

--- TrainCode/loss/custom_loss.py
+++ TrainCode/loss/custom_loss.py
@@ -5,33 +5,7 @@ import torch
 import torch.nn as nn
 import pickle
 
-def my_filter2D(image, kernel):
-    kx, ky = kernel.shape
-    x_n = kx//2
-    y_n = kx//2
-    padded_image = torch.nn.functional.pad(image, (0, 0, ky//2, ky//2, kx//2, kx//2))
-    filtimg = torch.Tensor(image)
-    px, py, nc = padded_image.shape
-    for x in range(px):
-        for y in range(py):
-            for c in range(nc):
-                if x - x_n < 0:
-                    continue
-                if x + x_n >= px:
-                    continue
-                if y - y_n < 0:
-                    continue
-                if y + y_n >= py:
-                    continue
-                total = 0.0
-                image_n = padded_image[x-x_n:x+x_n+1, y-y_n:y+y_n+1,c]
-                for x_i in range(kx):
-                    for y_i in range(ky):
-                        total += image_n[x_i,y_i]*kernel[x_i,y_i]
-                filtx = x-x_n
-                filty = y-y_n
-                filtimg[filtx,filty,c] = total
-    return filtimg
+
 
 class custom_loss(nn.Module):
     """Custom loss"""
@@ -40,7 +14,33 @@ class custom_loss(nn.Module):
         self.C1 = (0.01 * 255)**2
         self.C2 = (0.03 * 255)**2
         self.eps = 1e-3
-    
+    def my_filter2D(image, kernel):
+        kx, ky = kernel.shape
+        x_n = kx//2
+        y_n = kx//2
+        padded_image = torch.nn.functional.pad(image, (0, 0, ky//2, ky//2, kx//2, kx//2))
+        filtimg = torch.Tensor(image)
+        px, py, nc = padded_image.shape
+        for x in range(px):
+            for y in range(py):
+                for c in range(nc):
+                    if x - x_n < 0:
+                        continue
+                    if x + x_n >= px:
+                        continue
+                    if y - y_n < 0:
+                        continue
+                    if y + y_n >= py:
+                        continue
+                    total = 0.0
+                    image_n = padded_image[x-x_n:x+x_n+1, y-y_n:y+y_n+1,c]
+                    for x_i in range(kx):
+                        for y_i in range(ky):
+                            total += image_n[x_i,y_i]*kernel[x_i,y_i]
+                    filtx = x-x_n
+                    filty = y-y_n
+                    filtimg[filtx,filty,c] = total
+        return filtimg
         
     def forward(self,X, Y):
         i=0
@@ -53,14 +53,14 @@ class custom_loss(nn.Module):
             window=torch.tensor([[0.09474166, 0.11831801, 0.09474166],
            [0.11831801, 0.14776132, 0.11831801],
            [0.09474166, 0.11831801, 0.09474166]],device='cuda:0')
-            mu1=my_filter2D(X[i],window)[0][5:-5, 5:-5]
-            mu2=my_filter2D(Y[i],window)[0][5:-5, 5:-5]
+            mu1=self.my_filter2D(X[i],window)[0][5:-5, 5:-5]
+            mu2=self.my_filter2D(Y[i],window)[0][5:-5, 5:-5]
             mu1_sq=torch.multiply(mu1,mu1)
             mu2_sq=torch.multiply(mu2,mu2)
             mu1_mu2=torch.multiply(mu1,mu2)
-            sigma1_sq=my_filter2D(torch.multiply(X[i],X[i]),window)[0][5:-5,5:-5]-mu1_sq
-            sigma2_sq=my_filter2D(torch.multiply(Y[i],Y[i]),window)[0][5:-5,5:-5]-mu2_sq
-            sigma12=my_filter2D(torch.multiply(X[i],Y[i]),window)[0][5:-5,5:-5]-mu1_mu2
+            sigma1_sq=self.my_filter2D(torch.multiply(X[i],X[i]),window)[0][5:-5,5:-5]-mu1_sq
+            sigma2_sq=self.my_filter2D(torch.multiply(Y[i],Y[i]),window)[0][5:-5,5:-5]-mu2_sq
+            sigma12=self.my_filter2D(torch.multiply(X[i],Y[i]),window)[0][5:-5,5:-5]-mu1_mu2
             # sigma1_sq=my_filter2D(torch.multiply(X[i],X[i]),window)[5:-5, 5:-5] - mu1_sq
             # sigma2_sq=my_filter2D(torch.multiply(Y[i],Y[i]),window)[5:-5, 5:-5] - mu2_sq
             # sigma12=my_filter2D(torch.multiply(X[i],Y[i]),window)[5:-5, 5:-5] - mu1_mu2
