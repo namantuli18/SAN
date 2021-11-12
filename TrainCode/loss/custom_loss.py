0edee6b60f38a6a0d8392960d4dd92c2501ccc2e
Naman Tuli <63744860+namantuli18@users.noreply.github.com>
2021-11-12 19:35:46 +0530

    Update custom_loss.py

--- TrainCode/loss/custom_loss.py
+++ TrainCode/loss/custom_loss.py
@@ -11,48 +11,52 @@ class custom_loss(nn.Module):
         super(custom_loss, self).__init__()
         self.C1 = (0.01 * 255)**2
         self.C2 = (0.03 * 255)**2
-
     def forward(self, X, Y):
-        X=X.cpu()
-        Y=Y.cpu()
-        X=X.detach().numpy()
-        Y=Y.detach().numpy()
-        X=np.float64(X)
-        Y=np.float64(Y)
+        diff = torch.add(X, -Y)
+        error = torch.sqrt( diff * diff + self.eps )
+        loss = torch.sum(error)
+        return loss
+#     def forward(self, X, Y):
+#         X=X.cpu()
+#         Y=Y.cpu()
+#         X=X.detach().numpy()
+#         Y=Y.detach().numpy()
+#         X=np.float64(X)
+#         Y=np.float64(Y)
 
-        batch_loss=[]
-        for cnt,i in enumerate(X):
+#         batch_loss=[]
+#         for cnt,i in enumerate(X):
             
-            # print(img1[cnt].shape,img2[cnt].shape)
-            h=X[cnt].shape[2]
-            w=X[cnt].shape[1]
-            c=X[cnt].shape[0]
+#             # print(img1[cnt].shape,img2[cnt].shape)
+#             h=X[cnt].shape[2]
+#             w=X[cnt].shape[1]
+#             c=X[cnt].shape[0]
 
             
-            x=X[cnt].reshape((w,h,c))
-            y=Y[cnt].reshape((w,h,c))
-            x=x/255
-            y=y/255
-            kernel = cv2.getGaussianKernel(3, 1.5)
-            window = np.outer(kernel, kernel.transpose())
+#             x=X[cnt].reshape((w,h,c))
+#             y=Y[cnt].reshape((w,h,c))
+#             x=x/255
+#             y=y/255
+#             kernel = cv2.getGaussianKernel(3, 1.5)
+#             window = np.outer(kernel, kernel.transpose())
 
-            mu1 = cv2.filter2D(x, -1, window)[5:-5, 5:-5]  # valid
-            mu2 = cv2.filter2D(y, -1, window)[5:-5, 5:-5]
-            mu1_sq = mu1**2
-            mu2_sq = mu2**2
-            mu1_mu2 = mu1 * mu2
-            sigma1_sq = cv2.filter2D(x**2, -1, window)[5:-5, 5:-5] - mu1_sq
-            sigma2_sq = cv2.filter2D(y**2, -1, window)[5:-5, 5:-5] - mu2_sq
-            sigma12 = cv2.filter2D(x * y, -1, window)[5:-5, 5:-5] - mu1_mu2
-
-
+#             mu1 = cv2.filter2D(x, -1, window)[5:-5, 5:-5]  # valid
+#             mu2 = cv2.filter2D(y, -1, window)[5:-5, 5:-5]
+#             mu1_sq = mu1**2
+#             mu2_sq = mu2**2
+#             mu1_mu2 = mu1 * mu2
+#             sigma1_sq = cv2.filter2D(x**2, -1, window)[5:-5, 5:-5] - mu1_sq
+#             sigma2_sq = cv2.filter2D(y**2, -1, window)[5:-5, 5:-5] - mu2_sq
+#             sigma12 = cv2.filter2D(x * y, -1, window)[5:-5, 5:-5] - mu1_mu2
 
 
-            loss = (sigma1_sq*sigma2_sq-2*sigma12)
-            batch_loss.append(np.mean(loss))
-        #return ssim_map.mean() 
-        X=torch.from_numpy(X)
-        Y=torch.from_numpy(Y)
-        returner = torch.from_numpy(np.array(batch_loss)).sum()
-        returner.requires_grad=True
-        return returner
+
+
+#             loss = (sigma1_sq*sigma2_sq-2*sigma12)
+#             batch_loss.append(np.mean(loss))
+#         #return ssim_map.mean() 
+#         X=torch.from_numpy(X)
+#         Y=torch.from_numpy(Y)
+#         returner = torch.from_numpy(np.array(batch_loss)).sum()
+#         returner.requires_grad=True
+#         return returner
